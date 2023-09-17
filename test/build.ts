6cad5a4e98dde0bee56b0f648997e1d64700ee70
Skya Tura <antonio@skyatura.com.br>
2023-09-17 01:10:30 +0000

    Update build.ts

--- test/build.ts
+++ test/build.ts
@@ -2,7 +2,7 @@ await Bun.build({
     entrypoints: ['./index.tsx'],
     outdir: './out',
   })
-  
+  console.log(process.cwd)
   const input = Bun.file("./src/index.html");
   const output = Bun.file("./out/index.html");
   await Bun.write(output, input);
